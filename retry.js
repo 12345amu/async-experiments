61d4c7d372a84ac440c0aa847d06d7c61fe6db1b
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-24 00:12:15 +0530

    Add simulateFetch function to mimic

--- retry.js
+++ retry.js
@@ -1,3 +1,24 @@
 const async = require('async');
 
+let attempt = 0;
+
+function simulateFetch(callback) {
+  attempt++;
+  console.log(` Attempt ${attempt}: Fetching data from server...`); 
+
+  const success = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (success) {
+      console.log(" Data received!"); 
+      callback(null, { message: "Here is your data!" });
+    } else {
+      console.log(" Network error!"); 
+      callback(new Error("Network Error"));
+    }
+  }, 300);
+}
+
 console.log("Starting the data fetch with retry mechanism...");
+
+
