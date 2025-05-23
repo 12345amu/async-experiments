83790c42d97e5119ed777db579dc460f0e2465a0
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-23 18:17:03 +0530

    Setup CLI interface and require async/readline modules

---
+++ parallel.js
@@ -0,0 +1,8 @@
+const async = require('async');
+const readline = require('readline');
+
+// Create CLI interface
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
