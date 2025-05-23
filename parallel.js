4dc543ed1a9ddb5d4974c85c6ade9170f4afa6bc
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-23 18:51:30 +0530

    "Add snack preparation functions"

--- parallel.js
+++ parallel.js
@@ -7,3 +7,25 @@ if (selectedItems.length === 0) {
   console.log(' Please provide snacks to prepare: samosa, paneer, jalebi');
   process.exit(1);
 }
+
+// All available snack functions
+const snackMakers = {
+  samosa: function(callback) {
+    setTimeout(() => {
+      console.log(' Got samosa'); 
+      callback(null, 'samosa');
+    }, 1000);
+  },
+  paneer: function(callback) {
+    setTimeout(() => {
+      console.log(' Got paneer tikka'); 
+      callback(null, 'paneer tikka');
+    }, 500);
+  },
+  jalebi: function(callback) {
+    setTimeout(() => {
+      console.log(' Got jalebi'); 
+      callback(null, 'jalebi');
+    }, 1500);
+  }
+};
