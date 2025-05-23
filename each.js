ae2cc06ab1a81e79e94a28a301fe841eacd137c7
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-23 15:52:35 +0530

    add asyncSquare function with simulated delay

--- each.js
+++ each.js
@@ -2,4 +2,13 @@ const async = require('async');
 
 // Read numbers from command line
 const numbers = process.argv.slice(2).map(Number);
+
+function asyncSquare(num, callback) {
+  const delay = Math.floor(Math.random() * 1000);
+  setTimeout(() => {
+    console.log(`Processed ${num} after ${delay}ms`);
+    callback(null, num * num);
+  }, delay);
+}
+
 console.log('Input numbers:', numbers);
