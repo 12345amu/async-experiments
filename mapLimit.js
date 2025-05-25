ae058c7863f42f0b56667170ddfb38c5a0e312a2
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-25 23:22:01 +0530

    add simulated file download function

--- mapLimit.js
+++ mapLimit.js
@@ -6,3 +6,15 @@ if (files.length === 0) {
   console.error('Please provide at least one file name to download.');
   process.exit(1);
 }
+// Simulate file download
+function downloadFile(fileName, callback) {
+  console.log(`  Starting download: ${fileName}`);
+
+  // Simulated delay: 1-3 seconds
+  const delay = Math.floor(Math.random() * 2000) + 1000;
+
+  setTimeout(() => {
+    console.log(` Download complete: ${fileName}`);
+    callbak(null, `${fileName} downloaded`);
+  }, delay);
+}
