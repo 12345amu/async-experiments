d4a5d6af28caec7a9a69fd3e967f4c165b82370c
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-25 22:15:14 +0530

    Added CLI argument parsing to extract name and Aadhaar status

--- filter.js
+++ filter.js
@@ -4,7 +4,7 @@ const args = process.argv.slice(2);
 
 // Convert CLI args to array of people
 const people = args.map(arg => {
-  const [name, status] = arg.split(':');
+  const [name, status] = arg.split('=');
   return {
     name,
     hasAadhaar: status === 'true'
