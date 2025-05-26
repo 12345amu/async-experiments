f7dd59b1917dd34d33853f182794fe23b862cefb
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-26 19:42:47 +0530

    Use async.some to check if any input number is even

--- some.js
+++ some.js
@@ -6,3 +6,16 @@ if (inputNumbers.length === 0 || inputNumbers.some(isNaN)) {
   console.error('Please provide a list of numbers. Example: node some.js 1 3 5 8');
   process.exit(1);
 }
+async.some(inputNumbers, (num, callback) => {
+  setTimeout(() => {
+    const isEven = num % 2 === 0;
+    callback(null, isEven);
+  }, 100);
+}, (err, result) => {
+  if (err) {
+    console.error('Error:', err);
+    process.exit(1);
+  }
+
+  console.log('Is there at least one even number?', result ? 'Yes' : 'No');
+});
