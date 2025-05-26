7e9ae6949813511459d8da734f44c9b97802e075
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-26 17:56:39 +0530

    Add async.reject logic to remove even numbers asynchronously

--- reject.js
+++ reject.js
@@ -10,3 +10,16 @@ if (numbers.some(isNaN)) {
 }
 
 console.log('Parsed numbers:', numbers);
+async.reject(numbers, (number, callback) => {
+  const isEven = number % 2 === 0;
+  setTimeout(() => {
+
+  }, 100);
+}, (err, result) => {
+  if (err) {
+    console.error('Error:', err);
+    return;
+  }
+
+  console.log(' Odd numbers:', result);
+});
