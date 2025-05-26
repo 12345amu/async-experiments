964c2ad7f7194e297504e7a759e073f34b98316b
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-26 17:12:11 +0530

    Add CLI number parsing and validation

--- reduce.js
+++ reduce.js
@@ -1,3 +1,9 @@
 const async = require('async');
 
 const args = process.argv.slice(2);
+const numbers = args.map(Number);
+
+if (numbers.length === 0 || numbers.some(isNaN)) {
+  console.error('Please provide numbers as CLI arguments');
+  process.exit(1);
+}
