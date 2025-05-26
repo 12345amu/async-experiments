291a7db0204aa9bf8a064ffc5d86e4daeed16616
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-05-26 15:53:44 +0530

    add async grouping function by first letter

--- groupby.js
+++ groupby.js
@@ -7,3 +7,10 @@ if (names.length === 0) {
   console.error('Please provide names as CLI arguments');
   process.exit(1);
 }
+
+function groupByFirstLetter(name, callback) {
+  setTimeout(() => {
+    const key = name[0].toUpperCase(); 
+    callback( key);
+  }, 100);
+}
